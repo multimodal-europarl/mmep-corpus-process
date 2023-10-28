b7e59713601a33e872552b200c2da724f352c49a
Bob Borges <robert.d.borges@gmail.com>
2023-10-28 22:39:53 +0200

    feat:script to install pymmep

---
+++ scripts/utils/reinstall-pymmep.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+Reinstall pymmep to the working virtual environment.
+
+(This will be depreciated after release of pymmep on Pip)
+
+NB, this will take pymmep from whatever branch of the pymmep
+repo that's currently checked out locally.
+"""
+import argparse, shutil
+
+
+
+
+def main(args):
+    ignore = [
+        ".git",
+        ]
+    src = "pymmep"
+    dest = f"{args.env_path}lib/python{args.python_version}/site-packages/pymmep/"
+    shutil.copytree(src, dest,
+                    ignore = shutil.ignore_patterns(*ignore),
+                    dirs_exist_ok = True
+        )
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = __doc__)
+    parser.add_argument("-e", "--env-path",
+                        type=str,
+                        default="../mmepenv/",
+                        help="Path to working Python environment."
+        )
+    parser.add_argument("-p", "--python-version",
+                        type=str,
+                        default="3.11",
+                        help="Python version in working environment"
+        )
+    args = parser.parse_args()
+    main(args)
