a29a765738ddb6d716a324ce9e40bf013642ee60
Bob Borges <robert.d.borges@gmail.com>
2023-10-29 02:04:56 +0200

    feat:update elan-generated annotation IDs

---
+++ scripts/eaf-utils/update-annotation-ids.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+"""
+Change non-unique elan-generated ids to formatted uuids.
+"""
+from pymmep.eaf_utils import (
+        eaf_iterator,
+        get_tiers,
+        parse_eaf,
+        write_eaf,
+        xml_formatted_uuid,
+    )
+from lxml import etree
+import argparse
+
+
+
+
+def update_annotation_ids(eaf):
+    tiers = get_tiers(eaf)
+    D = {}
+    UUIDS = set()
+    num_ids = 0
+    for tier in tiers:
+        print(tier, tier.tag, tier.attrib.get("TIER_ID"), xml_formatted_uuid())
+        for annotation in tier:
+            ID = annotation[0].attrib.get("ANNOTATION_ID")
+            print(ID)
+            if ID.startswith("a"):
+                new_id = xml_formatted_uuid()
+                D[ID] = new_id
+                annotation[0].attrib["ANNOTATION_ID"] = new_id
+                num_ids += 1
+                UUIDS.add(new_id)
+    assert len(UUIDS) == num_ids
+    for tier in tiers:
+        if tier.attrib.get("LINGUISTIC_TYPE_REF") != "default-lt":
+            for annotation in tier:
+                ref = annotation[0].attrib.get("ANNOTATION_REF")
+                if ref.startswith("a"):
+                    annotation[0].attrib["ANNOTATION_REF"] = D[ref]
+    return eaf
+
+
+
+
+
+def main(args):
+    eaf_paths = sorted(list(eaf_iterator(
+            tx_dir="mmep-corpus/transcribed-audio",
+            start=args.start, end=args.end
+        )))
+
+    count = 0
+    for ep in eaf_paths:
+        print(ep)
+        eaf = parse_eaf(ep)
+        update_annotation_ids(eaf) #write this
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description = __doc__)
+    parser.add_argument("-s", "--start", type=int, default=200809, help="Start: YYYYMM")
+    parser.add_argument("-e", "--end", type=int, default=200912, help="End: YYYYMM")
+    args = parser.parse_args()
+    main(args)
